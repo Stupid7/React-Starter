d5409dbac344c88f58daf22ecab9622f870fa3f9
Stupid7 <sudieep661@gmail.com>
2018-06-05 13:36:12 +0545

    small change

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 
-const API_KEY = "AIzaSyCKiiQZBxaIcw01OKsyo_RpXdd83f0mwLw";
+const API_KEY = ;
 
 
 const App = () => {
